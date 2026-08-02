6f65b1f66d3b8cc0d2875e358a78ba819a7dd120
Hikari <108615941+GoodLight999@users.noreply.github.com>
2026-08-02 13:52:22 +0900

    Route watchdog through rebind helper

--- app/src/main/java/io/github/jd1378/otphelper/worker/PersistenceWatchdogWorker.kt
+++ app/src/main/java/io/github/jd1378/otphelper/worker/PersistenceWatchdogWorker.kt
@@ -1,11 +1,9 @@
 package io.github.jd1378.otphelper.worker
 
-import android.content.ComponentName
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import io.github.jd1378.otphelper.MyWorkManager
-import io.github.jd1378.otphelper.NotificationListener
 import io.github.jd1378.otphelper.PersistenceService
 import io.github.jd1378.otphelper.utils.AppLogger
 
@@ -18,14 +16,7 @@ class PersistenceWatchdogWorker(
   override suspend fun doWork(): Result {
     AppLogger.i("PersistenceWatchdog", "watchdog tick")
     PersistenceService.start(applicationContext)
-    if (NotificationListener.isNotificationListenerServiceEnabled(applicationContext)) {
-      try {
-        NotificationListener.requestRebind(
-            ComponentName(applicationContext, NotificationListener::class.java))
-      } catch (error: Throwable) {
-        AppLogger.e("PersistenceWatchdog", "requestRebind failed", error)
-      }
-    }
+    PersistenceService.requestListenerRebind(applicationContext)
     MyWorkManager.rebindListeners(applicationContext, true)
     return Result.success()
   }
